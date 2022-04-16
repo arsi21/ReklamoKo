7df9a136033db829cee17ad9f3ebe37c775a3039
Rouen Quirino <rouen2101@gmail.com>
2022-04-16 08:08:00 +0800

    add hover effect in inbox and notification icon

--- src/pending-complaints.php
+++ src/pending-complaints.php
@@ -21,12 +21,18 @@
             <div class="header__tool">
                 <ul>
                     <li id="inbox-btn" class="header__inbox__cont">
-                        <img class="header__inbox" src="assets/icons/inbox.svg" alt="Inbox icon">
+                        <svg class="header__inbox" xmlns="http://www.w3.org/2000/svg" width="34.873" height="26.154" viewBox="0 0 34.873 26.154">
+                            <path id="inbox" d="M0,3V29.154H34.873V3ZM9.623,14.521l-6.717,8.3V9.078l6.717,5.443ZM3.606,5.906H31.265L17.436,17.113,3.606,5.906ZM11.88,16.35l5.556,4.5L23,16.343l8.154,9.905H3.869Zm13.379-1.837,6.707-5.436V22.661l-6.707-8.147Z" transform="translate(0 -3)"/>
+                        </svg>
+
                         <span class="message-counter">2</span>
                     </li>
 
                     <li id="notif-btn" class="header__notif__cont">
-                        <img class="header__notif" src="assets/icons/notification.svg" alt="Notification icon">
+                        <svg class="header__notif" xmlns="http://www.w3.org/2000/svg" width="29.242" height="35.09" viewBox="0 0 29.242 35.09">
+                            <path id="notification" d="M21.007,30.7a4.483,4.483,0,0,1-4.344,4.386A4.559,4.559,0,0,1,12.235,30.7Zm.2-24.936a3.074,3.074,0,0,1-1.522-2.661v0a3.066,3.066,0,1,0-6.131,0v0a3.07,3.07,0,0,1-1.522,2.661C5.208,9.729,9.131,22.9,2,25.223V27.78H31.242V25.223C24.111,22.9,28.036,9.727,21.208,5.768ZM16.621,1.462a1.462,1.462,0,1,1-1.462,1.462A1.464,1.464,0,0,1,16.621,1.462ZM7.189,24.856c1.738-2.437,2.347-5.689,2.872-8.5.654-3.494,1.271-6.8,3.442-8.055a6.112,6.112,0,0,1,6.239,0c2.171,1.259,2.788,4.56,3.442,8.055.525,2.813,1.133,6.065,2.872,8.5Z" transform="translate(-2)"/>
+                        </svg>
+
                         <span class="notif-counter">5</span>
                     </li>
 
