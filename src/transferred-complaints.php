067b7dffe7f44405b5775148d15b89b7f78421ca
Rouen Quirino <rouen2101@gmail.com>
2022-04-17 19:09:37 +0800

    add all partials and content in other pages

---
+++ src/transferred-complaints.php
@@ -0,0 +1,190 @@
+<!-- include all needed partials -->
+<?php include 'partials/header.php';?>
+<?php include 'partials/navigation.php';?>
+
+
+
+
+        <section id="content" class="content">
+            <div class="content__title__cont">
+                <h2 class="content__title">Transferred Complaints</h2>
+            </div>
+
+            <div class="content__body__cont">
+                <form class="content__search__cont" action="">
+                    <button class="content__search__btn">
+                        <img src="assets/icons/search.svg" alt="Search icon">
+                    </button>
+
+                    <input class="content__search" id="search_input" type="search" name="search" placeholder="Search a complain">
+                </form>
+
+                <div class="content__pages__indicator">
+                    <div class="content__page__num">
+                        <span>1</span><span>-</span><span>50</span><span> of </span><span>100</span>
+                    </div>
+
+                    <div class="content__page__action">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10.583" height="16.934" viewBox="0 0 10.583 16.934">
+                            <path id="arrow" d="M5,2.117,7.157,0l8.427,8.467L7.157,16.934,5,14.817l6.35-6.35Z" transform="translate(15.583 16.934) rotate(180)" fill="#979797"/>
+                            </svg>
+                        </button>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10.583" height="16.934" viewBox="0 0 10.583 16.934">
+                            <path id="arrow" d="M5,2.117,7.157,0l8.427,8.467L7.157,16.934,5,14.817l6.35-6.35Z" transform="translate(-5)" fill="#403f3f"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <hr class="content__hr">
+
+                <div class="content__item__list__cont">
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi error magni quas architecto corporis natus, mollitia vitae suscipit provident tenetur expedita illo, dolorum rerum odit iusto facilis ipsa porro. Voluptatibus ratione reprehenderit dolore. Mollitia nisi laudantium nemo porro. Rerum, temporibus.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium amet, beatae assumenda mollitia veniam quas cumque perferendis asperiores. Quidem dolorum totam tempore laudantium quibusdam quasi natus cupiditate consequuntur eligendi saepe quis vitae, odit rem, tenetur vel recusandae ipsum enim molestias.</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium qui repellat iusto totam distinctio molestias maiores tempore possimus quasi doloribus ipsam nisi sequi dolore eum fuga at iste, consequuntur beatae. Exercitationem iure pariatur architecto corrupti praesentium, deserunt facere delectus aliquid!</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, fugit neque laboriosam explicabo, numquam quasi qui excepturi exercitationem assumenda incidunt, dolor repellat! Voluptatem, repudiandae officiis nesciunt neque, iste nobis ratione praesentium tenetur voluptate dolorem delectus tempora earum, id enim numquam?</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo optio omnis excepturi asperiores sapiente, amet velit, corporis nisi necessitatibus quas perferendis explicabo voluptates veniam qui est fugiat laboriosam ex beatae non saepe sit! Voluptate veniam fugiat doloremque. Natus, corporis!</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+
+                    <div class="content__item__cont">
+                        <div class="content__item__info__cont">
+                            <span class="content__item__name">Juan Dela Cruz</span>
+                            <span class="content__item__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quam excepturi quos mollitia eos debitis, magnam error commodi hic incidunt architecto fugiat dolore obcaecati eaque placeat non id est adipisci sunt deleniti quas velit optio voluptates? Natus obcaecati quisquam dolor?</span>
+                            <span class="content__item__date">01/10/22</span>
+                            <span class="content__item__status">Pending</span>
+                        </div>
+                    </div>
+                </div>
+                <hr class="content__hr">
+            </div>
+        </section>
+    </div>
+
+
+
+
+    
+
+<!-- include partials -->
+<?php include 'partials/footer.php';?>
