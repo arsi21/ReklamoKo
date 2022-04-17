d0a9fa7d6cb733555348463586d4ece59e0a8f48
Rouen Quirino <rouen2101@gmail.com>
2022-04-17 18:07:08 +0800

    add blank php for other pages

---
+++ src/dashboard.php
@@ -0,0 +1,6 @@
+<!-- include all needed partials -->
+<?php include 'partials/header.php';?>
+<?php include 'partials/navigation.php';?>
+
+<!-- include partials -->
+<?php include 'partials/footer.php';?>
