238a4ec09df0231d0fad336b6d7b80ef52b06cbe
Rouen Quirino <rouen2101@gmail.com>
2022-04-17 10:57:39 +0800

    add content page indicator in pending complaints

--- src/pending-complaints.php
+++ src/pending-complaints.php
@@ -19,6 +19,26 @@
                     <input class="content__search" id="search_input" type="search" name="search" placeholder="Search a complain">
                 </form>
 
+                <div class="content__pages__indicator">
+                    <div class="content__page__num">
+                        <span>1</span><span>-</span><span>50</span><span> of </span><span>100</span>
+                    </div>
+
+                    <div class="content__page__action">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10.583" height="16.934" viewBox="0 0 10.583 16.934">
+                            <path id="arrow" d="M5,2.117,7.157,0l8.427,8.467L7.157,16.934,5,14.817l6.35-6.35Z" transform="translate(15.583 16.934) rotate(180)" fill="#979797"/>
+                            </svg>
+                        </button>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10.583" height="16.934" viewBox="0 0 10.583 16.934">
+                            <path id="arrow" d="M5,2.117,7.157,0l8.427,8.467L7.157,16.934,5,14.817l6.35-6.35Z" transform="translate(-5)" fill="#403f3f"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <hr class="content__hr">
 
                 <div class="content__item__list__cont">
                     <div class="content__item__cont">
@@ -156,6 +176,7 @@
                         </div>
                     </div>
                 </div>
+                <hr class="content__hr">
             </div>
         </section>
     </div>
