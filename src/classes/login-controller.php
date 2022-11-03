a7e7fec57c4e9341221de42eacc9deebd812b987
Rouen Quirino <rouen2101@gmail.com>
2022-11-03 17:20:01 +0800

    add validation of input

--- src/classes/login-controller.php
+++ src/classes/login-controller.php
@@ -12,7 +12,17 @@ class LoginController extends Login {
 
     public function loginUser() {
         if(!$this->emptyInput()){
-            header("location: ../login.php?message=emptyInput&mobileNumber=$mobileNumber");
+            header("location: ../login.php?message=emptyInput&mobileNumber=$this->mobileNumber");
+            exit();
+        }
+
+        if(!$this->validMobileNumber()){
+            header("location: ../login.php?message=invalidMobileNumber");
+            exit();
+        }
+
+        if(!$this->validPassword()){
+            header("location: ../login.php?message=invalidPassword&mobileNumber=$this->mobileNumber");
             exit();
         }
 
@@ -24,6 +34,32 @@ class LoginController extends Login {
     private function emptyInput() {
         $result;
         if(empty($this->mobileNumber) || empty($this->password)){
+            $result = false;
+        }else {
+            $result = true;
+        }
+
+        return $result;
+    }
+
+    private function validMobileNumber() {
+        $result;
+        $pattern = "/^(09)\d{9}$/";
+
+        if(!preg_match($pattern, $this->mobileNumber)){
+            $result = false;
+        }else {
+            $result = true;
+        }
+
+        return $result;
+    }
+
+    private function validPassword() {
+        $result;
+        $pattern = "/^.{4,}$/";
+
+        if(!preg_match($pattern, $this->password)){
             $result = false;
         }else {
             $result = true;
--- src/login.php
+++ src/login.php
@@ -42,6 +42,27 @@
                             Mobile number and password didn't match.
                         </p>
                     </div>
+                <?php 
+                        }elseif($_GET['message'] == "invalidMobileNumber"){
+                ?>
+
+                    <div class="form-error">
+                        <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 21.534c5.518 0 9.998-4.48 9.998-9.998s-4.48-9.997-9.998-9.997c-5.517 0-9.997 4.479-9.997 9.997s4.48 9.998 9.997 9.998zm0-8c-.414 0-.75-.336-.75-.75v-5.5c0-.414.336-.75.75-.75s.75.336.75.75v5.5c0 .414-.336.75-.75.75zm-.002 3c-.552 0-1-.448-1-1s.448-1 1-1 1 .448 1 1-.448 1-1 1z" fill-rule="nonzero"/></svg>
+                        <p>
+                            Please enter valid mobile number. Example 09123456789.
+                        </p>
+                    </div>
+
+                <?php 
+                        }elseif($_GET['message'] == "invalidPassword"){
+                ?>
+
+                    <div class="form-error">
+                        <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 21.534c5.518 0 9.998-4.48 9.998-9.998s-4.48-9.997-9.998-9.997c-5.517 0-9.997 4.479-9.997 9.997s4.48 9.998 9.997 9.998zm0-8c-.414 0-.75-.336-.75-.75v-5.5c0-.414.336-.75.75-.75s.75.336.75.75v5.5c0 .414-.336.75-.75.75zm-.002 3c-.552 0-1-.448-1-1s.448-1 1-1 1 .448 1 1-.448 1-1 1z" fill-rule="nonzero"/></svg>
+                        <p>
+                            Password should be at least 4 digits or more.
+                        </p>
+                    </div>
 
                 <?php 
                         }elseif($_GET['message'] == "emptyInput"){
